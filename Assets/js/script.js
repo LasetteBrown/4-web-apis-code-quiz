6efb6128ffe8d2968b22a0cd61bf25844d2848a6
Lasette <lasette.brown@gmail.com>
2021-01-21 14:37:23 -0700

    created relevent quiz questions.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -78,33 +78,113 @@ function runGame() {
 
     //store each question into an object variable
     var qOne = {
-        question: "What is your name?",
-        choices: ["Lancelot",
-            "King Aurthur",
-            "Sir Robin",
-            "Galihad"],
-        correctAnswer: "King Aurthur",
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Making Languages",
+            "Holding Text and Markup Languages",
+            "Hyper Text Markup Language",
+            "How To Make Lasagna"],
+        correctAnswer: "Hyper Text Markup Language",
     };
     var qTwo = {
-        question: "What is your quest?",
-        choices: ["To find the Holy Grail",
-            "To fart in your general direction",
-            "To mock you a second time",
-            "To run away"],
-        correctAnswer: "To find the Holy Grail",
+        question: "Which element tag is used to create a link on a webpage?",
+        choices: ["<button>",
+            "<link>",
+            "<src>",
+            "<a>"],
+        correctAnswer: "&lt;a&gt;",
     };
     var qThree = {
-        question: "What is your favorite color?",
-        choices: ["Red",
-            "Purple",
-            "Green",
-            "Blue"],
-        correctAnswer: "Blue",
+        question: "What does the <head> tag do?",
+        choices: ["creates a header for the page",
+            "creates a nav bar on the page",
+            "holds meta-data for the page",
+            "holds content for the page"],
+        correctAnswer: "holds meta-data for the page",
+
+    };
+    var qFour = {
+        question: "Which creates the largest heading?",
+        choices: ["<h4>",
+            "<h1>",
+            "<header>",
+            "<head>"],
+        correctAnswer: "&lt;h1&gt;",
+
+    };
+    var qFive = {
+        question: "What does CSS stand for?",
+        choices: ["Cascading Style Sheet",
+            "Creative Style Sheet",
+            "Colorful Stlye Syntax",
+            "Cascading Style Solutions"],
+        correctAnswer: "Cascading Style Sheet",
+
+    };
+    var qSix = {
+        question: "Which character allows a CSS stylesheet to select an id?",
+        choices: [".",
+            "#",
+            "$",
+            "*"],
+        correctAnswer: "#",
+
+    };
+    var qSeven = {
+        question: "In CSS, Which is the correct syntax?",
+        choices: ["body: color=green;",
+            "{“body”, “color:green”}",
+            "body = color:green;",
+            "body {color:green;}"],
+        correctAnswer: "body {color:green;}",
+
+    };
+    var qEight = {
+        question: "How is a CSS stylesheet linked in html?",
+        choices: ["<a href=style.css>",
+            "<link rel=”stylesheet” href=style.css>",
+            "<style src=”style.css”>",
+            "<stylesheet>style.css</stylesheet>"],
+        correctAnswer: "&lt;link rel=”stylesheet” href=style.css&gt;",
+    };
+    var qNine = {
+        question: "In JavaScript, what is the correct syntax for creating a function?",
+        choices: ["function myFunction()",
+            "function = myfunction()",
+            "function: myFunction()",
+            "function “myFunction”()"],
+        correctAnswer: "function myFunction()",
+
+    };
+    var qTen = {
+        question: "In JavaScript, what does “var” do?",
+        choices: ["gives the variance property of an object",
+            "declares a variable",
+            "calls a function",
+            "verifies a function"],
+        correctAnswer: "declares a variable",
+
+    };
+    var qEleven = {
+        question: "Which is the correct way to start a for loop?",
+        choices: ["for (i=0; i<10; i++)",
+            "for(i= 1 to 5)",
+            "for (array.length)",
+            "for (array[i])"],
+        correctAnswer: "for (i=0; i&lt;10; i++)",
+
+    };
+    var qTwelve = {
+        question: "In JavaScript, which character is used to create an array?",
+        choices: ["{ }",
+            "< >",
+            "[ ]",
+            "( )"],
+        correctAnswer: "[ ]",
 
     };
 
     //place all questions into an array
-    var quizQuestions = [qOne, qTwo, qThree];
+    var quizQuestions = [qOne, qTwo, qThree, qFour, qFive, qSix, qSeven, qEight, qNine, qTen, qEleven, qTwelve];
     //a variable for indexing the array
     var currentQuestion = 0;
 
@@ -124,6 +204,7 @@ function runGame() {
 
             //elements created to show choices
             var olEl = document.createElement("ol");
+            olEl.setAttribute("style", "background-color:goldenrod; padding:10px; padding-left:20px;")
             olEl.setAttribute("type", "A");
             answersDiv.appendChild(olEl);
 
@@ -131,7 +212,7 @@ function runGame() {
             for (var i = 0; i < quizQuestions[currentQuestion].choices.length; i++) {
 
                 var liEl = document.createElement("li");
-                liEl.setAttribute("style", "background-color:goldenrod; padding:5px; margin:5px;");
+                liEl.setAttribute("style", "color:black; background-color:cornsilk; padding:5px; margin:5px;");
                 liEl.setAttribute("class", "hover-overlay");
                 liEl.textContent = quizQuestions[currentQuestion].choices[i];
                 olEl.appendChild(liEl);
@@ -141,6 +222,7 @@ function runGame() {
             //event listener for user selection
             olEl.addEventListener("click", function (event) {
 
+                console.log(event.target.innerHTML)
                 //if the user selection matches the correct answer
                 if (event.target.innerHTML === quizQuestions[currentQuestion].correctAnswer) {
                     //tells the user they got it right
